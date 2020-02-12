a0fb126c9c4ee713a93123895f2ccaf99c12d031
LukasKaselionis <l.kaselionis@gmail.com>
2020-02-12 12:22:17 +0200

    Implemented show/hide content div

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,7 +6,32 @@ function toggleNavigationMenu() {
     let button = document.getElementById('toggle-button');
     button.onclick = function() {
         nav.classList.toggle('menu-hidden');
-        menu.classList.toggle('menu-primary-hidden');
+        menu.classList.toggle('menu-primary-hidden')
         button.classList.toggle('menu-button-toggle');
     }
+}
+
+// Main-content
+
+let li_elements = document.querySelectorAll(".menu ul li");
+let item_elements = document.querySelectorAll(".content");
+for (let i = 0; i < li_elements.length; i++) {
+    li_elements[i].addEventListener("click", function() {
+        let li_value = this.getAttribute("data-li");
+        item_elements.forEach(function(item) {
+            item.style.display = "none";
+        });
+
+        if (li_value == "about") {
+            document.querySelector("." + li_value).style.display = "block";
+        } else if (li_value == "project") {
+            document.querySelector("." + li_value).style.display = "block";
+        } else if (li_value == "technologies") {
+            document.querySelector("." + li_value).style.display = "block";
+        } else if (li_value == "contact") {
+            document.querySelector("." + li_value).style.display = "block";
+        } else {
+            console.log("");
+        }
+    });
 }
