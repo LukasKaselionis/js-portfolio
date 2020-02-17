42247bff4c3e01d5de69edd594c70086912098a6
LukasKaselionis <l.kaselionis@gmail.com>
2020-02-17 17:35:01 +0200

    installing express.js and change all project structure

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+app.use(express.static('public'));
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
